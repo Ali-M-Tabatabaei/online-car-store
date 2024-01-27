5b4dea507d6a6465d6de9abdebf105fd6a240211
Ali Mohammad Tabatabaei <85901084+Ali-M-Tabatabaei@users.noreply.github.com>
2024-01-27 19:45:16 +0330

    updated settings.py

--- webApp/cardealer/settings.py
+++ webApp/cardealer/settings.py
@@ -94,12 +94,8 @@ WSGI_APPLICATION = 'cardealer.wsgi.application'
 
 DATABASES = {
      'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cardealer_db',
-        'USER': 'postgres', 
-        'PASSWORD': '0000', 
-        'HOST': '127.0.0.1',
-		'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
      }
  }
 
