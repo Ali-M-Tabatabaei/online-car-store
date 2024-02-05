0345575dd3814e83f79a5609c618ccc7d9960d36
SeyedMohammadMahdi <st.mohammad1380@gmail.com>
2024-02-05 11:29:54 +0330

    changed banner slider on home page jquery code

--- webApp/cardealer/static/js/app.js
+++ webApp/cardealer/static/js/app.js
@@ -142,41 +142,50 @@ $(function () {
     }
 
 
+    $("#carouselExampleIndicators").carousel({
+        interval: 7000
+    });
+
+    $("#carouselExampleIndicators").hover(function (){
+        $(this).carousel("cycle");
+    });
+
+
     // Banner slider
-    (function ($) {
-        //Function to animate slider captions
-        function doAnimations(elems) {
-            //Cache the animationend event in a variable
-            var animEndEv = 'webkitAnimationEnd animationend';
-            elems.each(function () {
-                var $this = $(this),
-                    $animationType = $this.data('animation');
-                $this.addClass($animationType).one(animEndEv, function () {
-                    $this.removeClass($animationType);
-                });
-            });
-        }
+    // (function ($) {
+    //     //Function to animate slider captions
+    //     function doAnimations(elems) {
+    //         //Cache the animationend event in a variable
+    //         var animEndEv = 'webkitAnimationEnd animationend';
+    //         elems.each(function () {
+    //             var $this = $(this),
+    //                 $animationType = $this.data('animation');
+    //             $this.addClass($animationType).one(animEndEv, function () {
+    //                 $this.removeClass($animationType);
+    //             });
+    //         });
+    //     }
 
-        //Variables on page load
-        var $myCarousel = $('#carousel-example-generic')
-        var $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
-        //Initialize carousel
-        $myCarousel.carousel();
+    //     //Variables on page load
+    //     var $myCarousel = $('#carousel-example-generic')
+    //     var $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+    //     //Initialize carousel
+    //     $myCarousel.carousel();
 
-        //Animate captions in first slide on page load
-        doAnimations($firstAnimatingElems);
-        //Pause carousel
-        $myCarousel.carousel('pause');
-        //Other slides to be animated on carousel slide event
-        $myCarousel.on('slide.bs.carousel', function (e) {
-            var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-            doAnimations($animatingElems);
-        });
-        $('#carousel-example-generic').carousel({
-            interval: 3000,
-            pause: "false"
-        });
-    })(jQuery);
+    //     //Animate captions in first slide on page load
+    //     doAnimations($firstAnimatingElems);
+    //     //Pause carousel
+    //     $myCarousel.carousel('pause');
+    //     //Other slides to be animated on carousel slide event
+    //     $myCarousel.on('slide.bs.carousel', function (e) {
+    //         var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+    //         doAnimations($animatingElems);
+    //     });
+    //     $('#carousel-example-generic').carousel({
+    //         interval: 3000,
+    //         pause: "false"
+    //     });
+    // })(jQuery);
 
     // Page scroller initialization.
     $.scrollUp({
